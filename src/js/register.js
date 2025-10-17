e22cf027e5262f69427de460deafe51a7d954c7e
Leo Ama <amadoreleoncio@gmail.com>
2025-10-17 17:40:37 +0800

    Hello

--- src/js/register.js
+++ src/js/register.js
@@ -242,3 +242,28 @@ document.addEventListener('DOMContentLoaded', () => {
             document.body.innerHTML = '<p style="color: white; text-align: center; padding-top: 50px;">Sorry, the page could not be loaded. Please try again later.</p>';
         });
 });
+
+const stopStyle = [
+  'font-size: 50px',
+  'font-weight: bold',
+  'color: red',
+  'padding: 10px 0'
+].join(';');
+
+const messageStyle = [
+  'font-size: 18px',
+  'line-height: 1.5'
+].join(';');
+
+
+console.log('%cStop!', stopStyle);
+
+console.warn(
+  '%cThis is a browser feature intended for developers. If someone told you to copy-paste something here to get free internet, increase your speed, or "hack" into our system, it is a scam. Doing so will give them access to your FiBear Network account and personal information.',
+  messageStyle
+);
+
+console.log(
+  '%cIf you have any questions or need support, please visit our official facebook: https://www.facebook.com/fntc.kasiglahanvillage2023 or contact our customer service directly. Never share your account details or paste code given to you by an untrusted source.',
+  messageStyle
+);
